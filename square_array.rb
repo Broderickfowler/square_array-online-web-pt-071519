d1369f59b7b36e55bdfb153b630d1c4a8ebb5f11
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-07-28 21:13:37 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-  # your code here
-  square_array = ["","",""]
-  square_array.each do |numbers|
-    puts "#{numbers}"
-  end
+ new_array = []
+ array.each do |x|
+  new_array << x*x # or x**2
+ end
+ new_array
 end
